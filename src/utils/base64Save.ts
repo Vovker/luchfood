2670b59402efc6bf39584c5de37e2009ec7fa5f5
Uladzimir Kavalchuk <vovkerg5@gmail.com>
2022-09-09 12:04:27 +0300

    fix: random id for save images

--- src/utils/base64Save.ts
+++ src/utils/base64Save.ts
@@ -2,7 +2,7 @@ import * as fs from "fs";
 
 export const base64Save = (base64: string) => {
   const base64Data = base64.replace(/^data:image\/jpeg;base64,/, "");
-  const fileName = `${Date.now()}.jpeg`;
+  const fileName = `${Date.now()+Math.floor(Math.random() * 100)}.jpeg`;
   const filePath = `${__dirname}/../${process.env.IMAGE_PATH}/${fileName}`;
   fs.writeFileSync(filePath, base64Data, 'base64');
   return fileName;
